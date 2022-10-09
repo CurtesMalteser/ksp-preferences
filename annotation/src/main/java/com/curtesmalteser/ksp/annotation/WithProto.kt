8e60428703fa45f59509a11c094d9bd95a50ca30
Curtes Malteser <antonio_bastiao@hotmail.com>
2022-10-09 22:45:25 +0200

    Adds protobuf

--- annotation/src/main/java/com/curtesmalteser/ksp/annotation/WithProto.kt
+++ annotation/src/main/java/com/curtesmalteser/ksp/annotation/WithProto.kt
@@ -4,5 +4,5 @@ package com.curtesmalteser.ksp.annotation
  * Created by António Bastião on 29.06.22
  * Refer to <a href="https://github.com/CurtesMalteser">CurtesMalteser github</a>
  */
-enum class PreferenceTarget {
-}
+@Target(AnnotationTarget.CLASS)
+annotation class WithProto
---
+++ app/src/main/java/com/curtesmalteser/ksp/preferences/data/MainRepository.kt
@@ -0,0 +1,48 @@
+package com.curtesmalteser.ksp.preferences.data
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import com.curtesmalteser.ksp.preferences.UserPreferences
+import java.io.InputStream
+import java.io.OutputStream
+
+/**
+ * Created by António Bastião on 09.10.22
+ * Refer to <a href="https://github.com/CurtesMalteser">CurtesMalteser github</a>
+ */
+interface MainRepository {
+    fun saveSettings()
+}
+
+class MainRepositoryImpl() : MainRepository {
+    override fun saveSettings() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+
+object UserPreferencesSerializer : Serializer<UserPreferences> {
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserPreferences = try {
+        UserPreferences.parseFrom(input)
+    } catch (exception: InvalidProtocolBufferException) {
+        throw CorruptionException("Cannot read proto.", exception)
+    }
+
+
+    override suspend fun writeTo(
+        t: UserPreferences,
+        output: OutputStream
+    ) = t.writeTo(output)
+}
+
+val Context.settingsDataStore: DataStore<UserPreferences> by dataStore(
+    fileName = "user_prefs.pb",
+    serializer = UserPreferencesSerializer
+)
